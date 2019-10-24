920c80356b95d299f2d272fdd5dcb73ac788150d
alexandre <alexandre@teste.com.br>
2019-10-24 20:27:39 -0300

    Classes iniciais

---
+++ AlunoDAO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhojava;
+
+
+/**
+ *
+ * @author 93580843087
+ */
+public class AlunoDAO implements OperacoesDAO{
+    @Override
+    public boolean pesquisar(Object obj){
+        return true;
+    }
+    @Override
+    public void inserir (Object obj) {
+        
+    }
+    @Override
+    public void excluir (Object obj) {
+        
+    }
+    @Override
+    public void editar (Object obj) {
+        
+    }
+}
---
+++ OperacoesDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhojava;
+
+/**
+ *
+ * @author 93580843087
+ */
+public interface OperacoesDAO {
+    public boolean pesquisar(Object obj);
+    public void inserir (Object obj);
+    public void excluir (Object obj);
+    public void editar (Object obj);
+}
