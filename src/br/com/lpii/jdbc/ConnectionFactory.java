5218031f588c201c90955ab5d13808cb413125db
Alexandre Lima <limaalexguitar@gmail.com>
2019-11-23 02:42:18 -0300

    23-11

---
+++ src/br/com/lpii/jdbc/ConnectionFactory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.lpii.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * Classe responsável por realizar a conexão com o Banco de dados
+ */
+public class ConnectionFactory {
+    
+    public Connection getConnection() {
+        
+        // try... catch. Para tratar eventuais erros na conexão
+        try {
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1/trabalho-java", "admin", "admin");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+}
---
+++ src/br/com/lpii/model/Pessoa.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.lpii.model;
+
+/**
+ *
+ * @author Alexandre Lima
+ */
+public class Pessoa {
+    
+    // Atributos
+    private String nome;
+    private String email;
+    private String senha;
+    
+    // Getters Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
